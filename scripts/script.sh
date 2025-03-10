2b49ff6bd9cf42ecccd48edf615e59cfec9e38ee
MaCaG0 <464548.clot@fje.edu>
2025-03-10 18:18:24 +0000

    Commit del Script ops

---
+++ scripts/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp /var/lib/jenkins/workspace/pj9f5a13/app1/*.html /home/vagrant/pj9f5a13_faseb/scripts
+cp /var/lib/jenkins/workspace/pj9f5a13/app1/*.php /home/vagrant/pj9f5a13_faseb/scripts
+cp /var/lib/jenkins/workspace/pj9f5a13/app1/*.css /home/vagrant/pj9f5a13_faseb/scripts
+exit 0
+
